1ed09bb96344c2eb05bab5f4a1e2d9e7f1d0d8a1
saloum45 <saloumfall45@gmail.com>
2025-09-12 22:26:48 +0000

    in progress

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import { Router } from '@angular/router';
 import { ApiService } from '../service/api/api.service';
+import { SharedModuleModule } from '../shared-module/shared-module-module';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule],
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,IonContent,IonicModule,ReactiveFormsModule],
 })
 export class LoginPage implements OnInit {
 
