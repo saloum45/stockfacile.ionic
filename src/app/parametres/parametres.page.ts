a6e7cf53e24374dcf1c4dfdb46b2448eb339bbc7
saloum45 <saloumfall45@gmail.com>
2025-09-13 12:18:37 +0000

    in progress

--- src/app/parametres/parametres.page.ts
+++ src/app/parametres/parametres.page.ts
@@ -4,15 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { addIcons } from 'ionicons';
 import { bagOutline } from 'ionicons/icons';
+import { NgSelectModule } from '@ng-select/ng-select';
 
 @Component({
   selector: 'app-parametres',
   templateUrl: './parametres.page.html',
   styleUrls: ['./parametres.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule,NgSelectModule]
 })
 export class ParametresPage implements OnInit {
+    selectedCar!: number;
+
+    cars = [
+        { id: 1, name: 'Volvo' },
+        { id: 2, name: 'Saab' },
+        { id: 3, name: 'Opel' },
+        { id: 4, name: 'Audi' },
+    ];
 
   constructor() {}
 
