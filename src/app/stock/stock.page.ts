ce664f5bdce5282c4b14687e7c09ee0c50138e17
saloum45 <saloumfall45@gmail.com>
2025-09-14 20:11:28 +0000

    in progress

--- src/app/stock/stock.page.ts
+++ src/app/stock/stock.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
 import { ApiService } from '../service/api/api.service';
 
 @Component({
@@ -9,7 +9,7 @@ import { ApiService } from '../service/api/api.service';
   templateUrl: './stock.page.html',
   styleUrls: ['./stock.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule]
 })
 export class StockPage implements OnInit {
 
