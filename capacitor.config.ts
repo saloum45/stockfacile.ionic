4db1e529ed21ccc4ee1c0effbd7aec69537b5b5e
saloum45 <saloumfall45@gmail.com>
2025-09-14 21:55:19 +0000

    in progress

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'stockfacile.ionic',
+  appId: 'chitaritech.stockfacile.com',
+  appName: 'Stock Facile',
   webDir: 'www'
 };
 
