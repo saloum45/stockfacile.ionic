c4082c81dfb5de63afcbd25638b754a998a4dcd1
saloum45 <saloumfall45@gmail.com>
2025-09-14 22:09:00 +0000

    in progress

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,17 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'chitaritech.stockfacile.com',
   appName: 'Stock Facile',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 1000, // Durée d'affichage (1 seconde)
+      backgroundColor: "#ffffffff", // Couleur de fond : blanc
+      androidScaleType: "CENTER", // Ajuste l'image au centre en conservant les proportions
+      showSpinner: false, // Désactive le spinner pour un affichage plus propre
+      splashFullScreen: true, // Active le mode plein écran
+      splashImmersive: true, // Supprime la barre de navigation/status pendant le splash
+    },
+  },
 };
 
 export default config;
